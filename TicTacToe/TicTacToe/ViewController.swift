f65dfbb5c06a2b8fb1c052c2f6f0bd9bf8b875a0
Anna Friemoth <annafriemoth@gmail.com>
2019-11-06 19:38:46 -0500

    added alternating turns

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -10,7 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
+    var activePlayer = 1 // x
+    
+    var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0] // 0-empty, 1-x, 2-0
+    
+   
+    
+    @IBAction func action(_ sender: UIButton) {
+        
+        let activePosition = sender.tag - 1
+        
+        if gameState[activePosition] == 0 {
+            
+            gameState[activePosition] = activePlayer
+            
+        if (activePlayer == 1) {
+            sender.setBackgroundImage(UIImage(named: "x"), for: .normal)
+            activePlayer = 2
+            
+        } else {
+            sender.setBackgroundImage(UIImage(named: "o"), for: .normal)
+                activePlayer = 1
+        }
+    }
+    }
+    
+    override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
