46f9be6c156810c309af4be4823b6042abfc71ca
Anna Friemoth <annafriemoth@gmail.com>
2019-11-08 07:09:35 -0500

    updated

--- TicTacToe/TicTacToe/TicTacToeBrain.swift
+++ TicTacToe/TicTacToe/TicTacToeBrain.swift
@@ -10,4 +10,48 @@ import Foundation
 
 class TicTacToeBrain {
   
+    
+    
+    
+   
+       
+    var matrix = [ ["_","_","_"],
+                   ["_","_","_"],
+                   ["_","_","_"] ]
+    
+    
+    // TODO - have a check game state
+    //      - horizontal check, vertical check, diagnal check, tie
+    func checkGameState() {
+        
+    }
+   
+
+    
+    // TODO - update game board/game state
+    //      - parameters - player, row and column
+    //      - with a switch player1 player2
+    
+    //    func updateGameState(player: Player ,row: Int, column: Int) {
+    //        switch {
+    //
+    //        }
+    //    }
+    
+    // TODO - enum Player:
+    enum Player: String {
+        case player1
+        case player2
+        
+        mutating func alternate() {
+            switch self {
+                
+            case .player1:
+                self = .player2
+                
+            case .player2:
+                self = .player1
+            }
+        }
+    }
 }
--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -10,36 +10,72 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var activePlayer = 1 // x
+    @IBOutlet weak var winnerLabel: UILabel!
+    
+    @IBOutlet weak var playAgain: UIButton!
+    
+    @IBAction func playAgain(_ sender: Any) {
+        activeGame = true
+        activePlayer = 1
+        gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    }
+    
+    var activeGame = true
+    
+  
     
     var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0] // 0-empty, 1-x, 2-0
     
+    let winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    
    
+    
+    @IBOutlet var gameButtons: [GameButton]!
+    
+    var activePlayer = 1 // x
     
     @IBAction func action(_ sender: UIButton) {
         
         let activePosition = sender.tag - 1
         
-        if gameState[activePosition] == 0 {
-            
+        if gameState[activePosition] == 0 && activeGame {
+
             gameState[activePosition] = activePlayer
             
         if (activePlayer == 1) {
-            sender.setBackgroundImage(UIImage(named: "x"), for: .normal)
+            sender.setBackgroundImage(UIImage(named: "x"), for: [])
             activePlayer = 2
             
         } else {
-            sender.setBackgroundImage(UIImage(named: "o"), for: .normal)
+            sender.setBackgroundImage(UIImage(named: "o"), for: [])
                 activePlayer = 1
         }
+            
+            for combination in winningCombinations {
+                if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]] {
+                    
+                    activeGame = false
+                    
+                    winnerLabel.isHidden = false
+                    playAgain.isHidden = false
+                    
+                    if gameState[combination[0]] == 1 {
+                        winnerLabel.text = "X wins!"
+                    } else {
+                        winnerLabel.text = "O wins!"
+                    }
+                }
+                
+                
+            }
     }
     }
     
     override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-
+    
+        winnerLabel.isHidden = true
+        playAgain.isHidden = true
+ 
 }
-
+}
